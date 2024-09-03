93045c692b21a5b084cc134f861498c2955cf65b
André de Moraes Costa <an.moraescosta@gmail.com>
2024-09-03 12:10:04 -0300

    Added --nocol, --extended flags and the -l parameter

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 #define FILEPATH_MAX_SIZE 128llu
-#define LINE_SIZE 16llu
+#define MAX_LINE_SIZE
 
 typedef unsigned char bool;
 #define true 1
@@ -19,38 +19,83 @@ enum Mode
     MODE_COUNT
 };
 
-bool hexdump(FILE* file, unsigned char mode)
+void printchar(char c, bool spaces)
+{
+    // ignore non printable characters
+    if (c < 31)
+    {
+        if (spaces) printf(" . ");
+        else printf(".");
+    }
+    else 
+    {
+        if (spaces) printf(" %c ", c);
+        else printf("%c", c);
+    }
+}
+
+bool hexdump(FILE* file, size_t line_size, unsigned char mode, bool col_indicies, bool extended)
 {
     size_t filesize = 0llu; // file size in bytes
+
+    printf("line size: %zu\n", line_size);
+
+    if (line_size <= 0)
+    {
+        fprintf(stderr, "Invalid line size %zu\n", line_size);
+        exit(1);
+    }
+
+    byte* line = NULL;
+    line = calloc(line_size, sizeof(byte));
+
+    if (line == NULL)
+    {
+        fprintf(stderr, "Failed to allocate line buffer\n");
+        exit(1);
+    }
 
     fseek(file, 0, SEEK_END); // find end of file
     filesize = ftell(file);
     fseek(file, 0, SEEK_SET); // back to beggining of file
 
-    size_t num_lines = (size_t)(filesize / LINE_SIZE) + 1;
+    size_t num_lines = (size_t)(filesize / line_size) + 1;
 
     printf("[mode %u]: ", mode);
-    for (size_t i = 0; i < LINE_SIZE; i++)
+
+    if (col_indicies)
     {
-        printf("%02llX ", i);
+        for (size_t i = 0; i < line_size; i++)
+        {
+            printf("%02zX ", i);
+        }
     }
 
     printf("\n");
-
+    
     switch (mode) 
     {
         case MODE_BINARY:
         {
             for (size_t i = 0; i < num_lines; i += 1)
             {
-                byte line[LINE_SIZE] = { 0x00 };
-                fread(line, sizeof(byte), LINE_SIZE, file); 
+                fread(line, sizeof(byte), line_size, file); 
 
-                printf("%08llX: ", i * LINE_SIZE);
+                printf("%08zX: ", i * line_size);
 
-                for (size_t j = 0; j < LINE_SIZE; j++)
+                for (size_t j = 0; j < line_size; j++)
                 {
                     printf("%02hhX ", line[j]);
+                }
+
+                if (extended)
+                {
+                    printf("   | ");
+                    for (size_t j = 0; j < line_size; j++)
+                    {
+                        printchar(line[j], 0);
+                    }
+                    printf(" | ");
                 }
 
                 printf("\n");
@@ -62,24 +107,25 @@ bool hexdump(FILE* file, unsigned char mode)
         {
             for (size_t i = 0; i < num_lines; i += 1)
             {
-                byte line[LINE_SIZE] = { 0x00 };
-                fread(line, sizeof(byte), LINE_SIZE, file); 
+                fread(line, sizeof(byte), line_size, file); 
 
-                printf("%08llX: ", i * LINE_SIZE);
+                printf("%08zX: ", i * line_size);
 
-                for (size_t j = 0; j < LINE_SIZE; j++)
+                for (size_t j = 0; j < line_size; j++)
                 {
-                    char c = (char)line[j];
-                    // ignore non printable characters
-                    if (c < 31)
-                    {
-                        printf(" . ");
-                    }
-                    else 
-                    {
-                        printf(" %c ", line[j]);
-                    }
+                    printchar(line[j], col_indicies);
                 }
+
+                if (extended)
+                {
+                    printf("   | ");
+                    for (size_t j = 0; j < line_size; j++)
+                    {
+                        printf("%02hhX ", line[j]);
+                    }
+                    printf(" | ");
+                }
+
                 printf("\n");
             }
             break;
@@ -88,10 +134,12 @@ bool hexdump(FILE* file, unsigned char mode)
         default:
         {
             fprintf(stderr, "Invalid mode %u\n", mode);
+            free(line);
             return false;
         }
     }
 
+    free(line);
     return true;   
 }
 
@@ -101,12 +149,17 @@ void usage(void)
     printf("    hxdump <file/filepath> <arguments> <flags>\n");
     printf("\n");
     printf("    arguments:\n");
-    printf("     -h     prints usage (what you are currently reading)\n");
+    printf("     -h         prints usage (what you are currently reading)\n");
+    printf("     -l <num>   changes size of line\n");
     printf("\n");
     printf("    flags:\n");
-    printf("     --binary   open files in binary mode (mode 0)\n");
-    printf("     --ascii    open files in ascii mode  (mode 1)\n");
-
+    printf("      modes:\n");
+    printf("       --binary   open files in binary mode (mode 0)\n");
+    printf("       --ascii    open files in ascii mode  (mode 1)\n");
+    printf("\n");
+    printf("      other:\n");
+    printf("       --nocol     no column indicies\n");
+    printf("       --extended  print ascii/binary representation\n");
     printf("\n");
 }
 
@@ -145,27 +198,65 @@ int main(int argc, char**argv)
 
     // reaching this point means the file *should* be valid, congratulations!
 
-    unsigned char mode = 0;
+
+    // flag parsing
+    unsigned char mode = MODE_BINARY;
+    bool col = true;
+    bool extended = false;    
 
     const size_t max_read = 12;
-    if (argc == 3)
+    if (argc > 3)
     {
-        if (strncmp(argv[2], "--binary", max_read) == 0)
+        for (int i = 0; i < argc; i++)
         {
-            mode = MODE_BINARY;
-        }
-        else if (strncmp(argv[2], "--ascii", max_read) == 0)
-        {
-            mode = MODE_ASCII;
-        }
-        else
-        {
-            fprintf(stderr, "Invalid flag %s\n", argv[2]);
-            exit(1);
+            if (strncmp(argv[i], "--binary", max_read) == 0)
+            {
+                mode = MODE_BINARY;
+            }
+            else if (strncmp(argv[i], "--ascii", max_read) == 0)
+            {
+                mode = MODE_ASCII;
+            }
+            else if (strncmp(argv[i], "--nocol", max_read) == 0)
+            {
+                col = false;
+            }
+            else if (strncmp(argv[i], "--extended", max_read) == 0)
+            {
+                extended = true;
+            }
         }
     }
 
-    if (!hexdump(file, mode))
+    int line_size = 16;
+
+    // argument parsing
+
+    if (argc > 2)
+    {
+        if (strncmp(argv[2], "-l", max_read) == 0)
+        {
+            if (argc <= 3)
+            {
+                fprintf(stderr, "No specified line size, exiting\n");
+                exit(1);
+            }
+
+            line_size = strtol(argv[3], NULL, 10);
+            if (line_size == 0)
+            {
+                fprintf(stderr, "%s is not an integer value or is invalid\n", argv[3]);
+                exit(1);
+            } 
+            else if (line_size < 0)
+            {
+                fprintf(stderr, "%s is a negative number and is not a valid line size\n", argv[3]);
+                exit(1);
+            }
+        }
+    }
+
+    if (!hexdump(file, (size_t)line_size, mode, col, extended))
     {
         fprintf(stderr, "Error dumping hexadecimal data\n");
         exit(1);
